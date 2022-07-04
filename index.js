8053b0f852f85a35cfd22b3eebd1f19d47a2bd24
Ian Iversen <ianiversen1@gmail.com>
2022-07-04 16:06:24 -0400

    commit tree fee941850c863717f38454fa4b2beb0a20fda12c

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+document.getElementById("main").remove()
+
+const newHeader = document.createElement("h1")
+
+document.body.append(newHeader)
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML = 'Ian is the champion';
+
+
